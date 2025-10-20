e8808c7db170dd39acc837b56916162b3084783e
Cyn0705 <2216629610@qq.com>
2025-10-20 08:01:03 +0800

    Add files via upload

---
+++ 个人信息.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() 
+{
+    char name[] = "Student";
+    int age = 20;
+    printf("Name: %s\n", name);
+    printf("Age: %d\n", age);
+    printf("Welcome to C Programming!\n");
+    return 0;
+}
